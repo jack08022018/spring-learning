6989d70ed77a4ff64f1f79a7432d6649f014bc82
jack08022018 <dinhtienhoang2009@gmail.com>
2022-09-20 16:13:04 +0700

    sd

--- spring-validate/src/main/kotlin/com/springvalidate/controller/ApiController.kt
+++ spring-validate/src/main/kotlin/com/springvalidate/controller/ApiController.kt
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -25,8 +26,12 @@ class ApiController {
 
     }
 
-//    @PostMapping("/createPerson")
-//    fun createPerson(@Valid @RequestBody request: PersonRequest, bindingResult: BindingResult) {
+//    @PostMapping("/createPersonBinding")
+//    fun createPerson(@Valid @RequestBody request: PersonRequest, bindingResult: BindingResult): Any? {
+//        if(bindingResult.hasErrors()) {
+//            return bindingResult.getFieldErrors().associateBy({it -> (it as FieldError).field}, {it.defaultMessage})
+//        }
+//        return null
 //    }
 
 }
