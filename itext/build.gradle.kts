5406cd8334837655eac1b1bfa4a748a641bad292
jack08022018 <dinhtienhoang2009@gmail.com>
2022-10-12 17:35:01 +0700

    sd

--- itext/build.gradle.kts
+++ itext/build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.0"
+    kotlin("plugin.jpa") version "1.6.21"
     id("org.springframework.experimental.aot") version "0.12.1"
 }
 
 group = "com"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_17
+//java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
