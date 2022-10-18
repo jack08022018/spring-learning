63d9bd375c95e4a281f7cc64f4535d4ea7996947
dthoang1 <jack08022018>
2022-10-18 17:52:46 +0700

    aa

--- spring-aop/src/main/java/com/springaop/algorithm/java8/TaskService.java
+++ spring-aop/src/main/java/com/springaop/algorithm/java8/TaskService.java
@@ -4,16 +4,9 @@ import java.util.*;
 
 public class TaskService {
 	
-	private static Comparator<SessionInfo> timeoutComparator = new Comparator<SessionInfo>() {
-
-		@Override
-		public int compare(SessionInfo o1, SessionInfo o2) {
-			return (int) (o2.getStartTime() - o1.getStartTime());
-		}
-
-	};
+	private static final Comparator<SessionInfo> timeoutComparator = (o1, o2) -> (int) (o2.getStartTime() - o1.getStartTime());
 	
-	private static PriorityQueue<SessionInfo> taskQueue = new PriorityQueue<SessionInfo>(timeoutComparator);
+	private static final PriorityQueue<SessionInfo> taskQueue = new PriorityQueue<>(timeoutComparator);
 	
 	public static void main(String[] args) {
 		taskQueue.add(new SessionInfo("a", new Date().getTime()/1000));
@@ -22,17 +15,18 @@ public class TaskService {
 		taskQueue.add(new SessionInfo("d", new Date().getTime()/1000));
 		
 		SessionInfo a = taskQueue.poll();
+		System.out.println(a.getSessionId());
 		System.out.println(new Date().getTime()/1000);
 		
-		List<SessionInfo> list = Arrays.asList(new SessionInfo("b", 0), new SessionInfo("a", 0), new SessionInfo("c", 0));
-		
-		Comparator<SessionInfo> compare = new Comparator<SessionInfo>() {
-			@Override
-			public int compare(SessionInfo o1, SessionInfo o2) {
-				return o1.getSessionId().compareTo(o2.getSessionId());
-			}
-		};
-		int index = Collections.binarySearch(list, new SessionInfo("a", 0), compare);
-		System.out.println(index);
+//		List<SessionInfo> list = Arrays.asList(new SessionInfo("b", 0), new SessionInfo("a", 0), new SessionInfo("c", 0));
+//
+//		Comparator<SessionInfo> compare = new Comparator<SessionInfo>() {
+//			@Override
+//			public int compare(SessionInfo o1, SessionInfo o2) {
+//				return o1.getSessionId().compareTo(o2.getSessionId());
+//			}
+//		};
+//		int index = Collections.binarySearch(list, new SessionInfo("a", 0), compare);
+//		System.out.println(index);
 	}
 }
