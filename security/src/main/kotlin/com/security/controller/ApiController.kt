73bbcf28b7f1a6ae893f9386a90ed923f60faa60
jack08022018 <dinhtienhoang2009@gmail.com>
2022-10-20 13:43:05 +0700

    Revert "sd"
    This reverts commit aea63bbcc4be1d2edf3bc2984e6862f1536ac06c.
    
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/gradle.xml
    #	.idea/inspectionProfiles/Project_Default.xml
    #	multi-db/build.gradle
    #	multi-db/src/main/java/com/multidb/configuration/BeanConfig.java
    #	multi-db/src/main/java/com/multidb/controller/ApiController.java
    #	multi-db/src/main/java/com/multidb/enums/Gender.java
    #	multi-db/src/main/java/com/multidb/repository/employee/SalariesRepository.java
    #	multi-db/src/main/java/com/multidb/service/ApiService.java
    #	multi-db/src/main/java/com/multidb/service/impl/ApiServiceImpl.java
    #	multi-db/src/main/resources/application.yml

--- security/src/main/kotlin/com/security/controller/ApiController.kt
+++ security/src/main/kotlin/com/security/controller/ApiController.kt
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
