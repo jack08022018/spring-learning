5dd1cee2abefc5a6a375206e45c9295911ad5a69
dthoang1 <jack08022018>
2022-10-24 17:59:14 +0700

    aa

--- spring-aop/src/main/java/com/springaop/multithread/ThreadPoolDemo.java
+++ spring-aop/src/main/java/com/springaop/multithread/ThreadPoolDemo.java
@@ -1,14 +1,25 @@
 package com.springaop.multithread;
 
-import java.text.SimpleDateFormat;
+import com.springaop.multithread.threads.MyCallable;
+import com.springaop.multithread.threads.MyMonitorThread;
+import com.springaop.multithread.threads.Worker;
+import com.springaop.multithread.threads.WorkerThread;
+
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadPoolDemo {
     static final int MAX_THREAD = 2;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//        executorServiceExample();
+        threadPoolExecutorExample();
+//        executorServiceWithCalableFuture();
+//        futureTaskExample();
+    }
+    static void executorServiceExample() {
         ReentrantLock lock = new ReentrantLock();
         ExecutorService pool = Executors.newFixedThreadPool(MAX_THREAD);
         Runnable w1 = new Worker(lock, "Job1");
@@ -21,59 +32,72 @@ public class ThreadPoolDemo {
         pool.execute(w4);
         pool.shutdown();
     }
-
-    static class Worker implements Runnable {
-        String name;
-        ReentrantLock lock;
-        public Worker(ReentrantLock lock, String name) {
-            this.lock = lock;
-            this.name = name;
+    static void executorServiceWithCalableFuture() {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<Future<String>> list = new ArrayList<>();
+        Callable callable = new MyCallable(1000);
+        for(int i=0; i< 100; i++){
+            Future future = executor.submit(callable);
+            list.add(future);
         }
-        public void run() {
-            boolean done = false;
-            while (!done) {
-                if(lock.tryLock()) {
-                    try {
-                        System.out.println("task name - "+ name + " outer lock acquired at "
-                                + getCurrentDateFormat() + " Doing outer work");
-                        Thread.sleep(1500);
-
-                        lock.lock();
-                        try {
-                            System.out.println("task name - "+ name + " inner lock acquired at "
-                                    + getCurrentDateFormat() + " Doing inner work");
-                            System.out.println("Lock Hold Count - "+ lock.getHoldCount());
-                            Thread.sleep(1500);
-                        }catch(InterruptedException e) {
-                            e.printStackTrace();
-                        }finally {
-                            System.out.println("task name - " + name + " releasing inner lock");
-                            lock.unlock();
-                        }
-                        System.out.println("Lock Hold Count - " + lock.getHoldCount());
-                        System.out.println("task name - " + name + " work done");
-                        done = true;
-                    }catch(InterruptedException e) {
-                        e.printStackTrace();
-                    }finally {
-                        System.out.println("task name - " + name + " releasing outer lock");
-                        lock.unlock();
-                        System.out.println("Lock Hold Count - " + lock.getHoldCount());
-                    }
-                }else {
-                    System.out.println("task name - " + name + " waiting for lock");
-                    try {
-                        Thread.sleep(1000);
-                    }catch(InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+        for(Future<String> fut : list){
+            try {
+                System.out.println(new Date()+ "::"+fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         }
+        executor.shutdown();
+    }
+    static void futureTaskExample() {
+        FutureTask<String> futureTask1 = new FutureTask<>(new MyCallable(1000));
+        FutureTask<String> futureTask2 = new FutureTask<>(new MyCallable(2000));
 
-        private static String getCurrentDateFormat() {
-            return new SimpleDateFormat("hh:mm:ss").format(new Date());
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(futureTask1);
+        executor.execute(futureTask2);
+        while (true) {
+            try {
+                if(futureTask1.isDone() && futureTask2.isDone()){
+                    System.out.println("Done");
+                    executor.shutdown();
+                    return;
+                }
+                if(!futureTask1.isDone()){
+                    System.out.println("FutureTask1 output="+futureTask1.get());
+                }
+
+                System.out.println("Waiting for FutureTask2 to complete");
+                String s = futureTask2.get(200L, TimeUnit.MILLISECONDS);
+                if(s !=null){
+                    System.out.println("FutureTask2 output="+s);
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }catch(TimeoutException e){
+                //do nothing
+            }
         }
     }
+    static void threadPoolExecutorExample() throws InterruptedException {
+        ThreadPoolExecutor executorPool = new ThreadPoolExecutor(
+                2, 4,
+                10, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(2),
+                Executors.defaultThreadFactory(),
+                (r, executor) -> System.out.println(r.toString() + " is rejected"));
+        MyMonitorThread monitor = new MyMonitorThread(executorPool, 3);
+        Thread monitorThread = new Thread(monitor);
+        monitorThread.start();
+        for(int i=0; i<10; i++) {
+            executorPool.execute(new WorkerThread("cmd"+i));
+        }
+
+        Thread.sleep(30000);
+        executorPool.shutdown();
+        Thread.sleep(5000);
+        monitor.shutdown();
+    }
+
 }
 
